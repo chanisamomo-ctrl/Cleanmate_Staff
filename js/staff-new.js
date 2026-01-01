e985678ec64b791bd2440144154131bc41535a8e
chanisamomo-ctrl <chanisa.momo@gmail.com>
2026-01-01 20:38:25 +0700

    Update staff-new.js

--- js/staff-new.js
+++ js/staff-new.js
@@ -1,4 +1,11 @@
 // js/staff-new.js
+function todayYMD() {
+  const d = new Date();
+  const y = d.getFullYear();
+  const m = String(d.getMonth() + 1).padStart(2, "0");
+  const day = String(d.getDate()).padStart(2, "0");
+  return `${y}-${m}-${day}`;
+}
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("saveBtn");
   const result = document.getElementById("result");
